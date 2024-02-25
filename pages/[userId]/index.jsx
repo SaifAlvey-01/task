a8dec9f427011ded88c289deb3720a053b700080
SaifAlvey-01 <saif.alvey@gmail.com>
2024-02-25 11:48:25 +0500

    done.

--- pages/[userId]/index.jsx
+++ pages/[userId]/index.jsx
@@ -1,8 +1,9 @@
-'use client'
-import React, {useEffect, useState} from 'react';
-import { useRouter } from 'next/router';
-import UserProfilePage from '../../components/UserProfilePage';
-import Header from "../../components/Header"
+"use client";
+import React, { useEffect, useState } from "react";
+import { useRouter } from "next/router";
+import UserProfilePage from "../../components/UserProfilePage";
+import Lottie from "lottie-react";
+import pageload from "/public/Loader.json";
 
 const Profile = () => {
   const router = useRouter();
@@ -13,11 +14,13 @@ const Profile = () => {
     // Fetch user data based on userId
     const fetchUser = async () => {
       try {
-        const response = await fetch(`https://randomuser.me/api/?uuid=${userId}`);
+        const response = await fetch(
+          `https://randomuser.me/api/?uuid=${userId}`
+        );
         const data = await response.json();
         setUser(data.results[0]);
       } catch (error) {
-        console.error('Error fetching user data:', error);
+        console.error("Error fetching user data:", error);
       }
     };
 
@@ -27,7 +30,17 @@ const Profile = () => {
   }, [userId]);
 
   if (!user) {
-    return <div>Loading...</div>; // or display a loading indicator
+    return(
+    <div
+      style={{ backgroundColor: "#F7F9FB" }}
+      className="bg-[url('/partner_assets/bg-gradient.svg')] flex h-screen overflow-hidden"
+    >
+      <div className=" overflow-y-auto flex flex-col items-center justify-center w-full">
+        <div className="h-1/2 w-96 mx-auto my-auto">
+          <Lottie animationData={pageload} loop={true} autoplay={true} />;
+        </div>
+      </div>
+    </div>);
   }
 
   return (
